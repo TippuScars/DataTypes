d8f928c216a706351772f03a3b371ea80089de86
TippuScars <Tippuscars@gmail.com>
2024-03-24 21:27:44 +0530

    datatypes

---
+++ src/DataTypes1.java
@@ -0,0 +1,18 @@
+package DataTypes
+public class DataTypes1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x="10";
+		int y="20";
+		int z;
+		z=x+y;
+		System.out.println("Adding two numbers is"+z);
+		float b="0.222f";
+		float a="2.33f";
+		float c=a+b;
+		System.out.println("The float value is");
+
+	}
+
+}
